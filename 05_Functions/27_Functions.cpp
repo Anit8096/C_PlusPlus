54fe7423b4f0c57c3ad6d9eb44f2e3acd0829ba2
Anit <anitbani8096@gmail.com>
2024-07-16 21:31:16 +0530

    correction

--- 05_Functions/27_Functions.cpp
+++ 05_Functions/27_Functions.cpp
@@ -1,11 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int sum(int a, int b){
+
+int sum(int a, int b) {
    int Sum = a + b;
    cout << "Sum = " << Sum;
    return Sum;
 }
-int main(){
+
+int main() {
     int num1 = 1, num2 = 3;
     sum(num1,num2);
 }
--- 05_Functions/28_CallByValue.cpp
+++ 05_Functions/28_CallByValue.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
+
 void Swap(int a, int b){
     int temp = a;
     a = b;
     b = temp;
 }
+
 int main(){                 // method in C++ to pass the values to the function arguments. 
     int num1 = 3, num2 = 7; // In the case of call by value the copies of actual parameters 
     cout << num1 << num2;   // are sent to the formal parameter, which means that if we change 
